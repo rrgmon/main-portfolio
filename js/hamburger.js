f1e3e9d671b2719fce73393853225783f4a9d81a
RohitRegimon <rrgmon@protonmail.com>
2020-02-01 18:27:30 +0530

    Initial Commit

--- js/hamburger.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navLinks = document.getElementsByClassName("nav-list")[0];
-
-toggleButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
