20967413d1205b71f1d394b622bf2f2c16ace7bb
RohitRegimon <rrgmon@protonmail.com>
2021-06-22 00:15:24 +0530

    Installed express and ejs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-'use strict';
+// 'use strict';
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const PORT = process.env.port || 3000;
+
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '/views'));
+
+// app.get('/', (req, res) => {
+//   res.render('home.ejs');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on ${PORT}`);
+// });
