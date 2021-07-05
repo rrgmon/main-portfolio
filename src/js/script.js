e4375d436b1f813dacd2e66b8ca1bc2c3d002911
RohitRegimon <rrgmon@protonmail.com>
2021-07-05 16:43:33 +0530

    Added current day text, fontawesome icons, hover sizing

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
+// Projects card hover
 const cardEl = document.getElementsByClassName('card');
 const cardPopEl = document.getElementsByClassName('card-pop');
-const mediaQueryEl = window.matchMedia('(max-width: 900px)');
 
-function cardPopAdd() {
+(function cardPopAdd() {
   for (let i = 0; i < cardEl.length; i++) {
     cardEl[i].addEventListener('mouseover', () => {
       cardPopEl[i].classList.add('card-pop-display');
@@ -14,9 +14,9 @@ function cardPopAdd() {
       cardPopEl[i].classList.remove('card-pop-display');
     });
   }
-}
+})();
 
-function cardPopRemove() {
+(function cardPopRemove() {
   for (let i = 0; i < cardEl.length; i++) {
     cardEl[i].removeEventListener('mouseover', () => {
       cardPopEl[i].classList.add('card-pop-display');
@@ -26,14 +26,20 @@ function cardPopRemove() {
       cardPopEl[i].classList.remove('card-pop-display');
     });
   }
-}
+})();
 
-function widthDetect(mediaQueryEl) {
-  if (mediaQueryEl.matches) {
-    cardPopAdd();
-  } else {
-    cardPopRemove();
-  }
-}
+// Day text generator
 
-mediaQueryEl.addEventListener('onload change', widthDetect(mediaQueryEl));
+const dayTextEl = document.querySelector('.current-day');
+let date = new Date();
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+let currentDay = date.getDay();
+dayTextEl.textContent = days[currentDay];
