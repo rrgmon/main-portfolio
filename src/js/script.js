a82b00c8ea0a1989dc73231212cccab71255975e
rrgmon <rrgmon@protonmail.com>
2022-08-18 17:11:38 -0400

    Changed formatting

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,13 @@
 const dayTextEl = document.querySelector('.current-day');
 let date = new Date();
 const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
 ];
 let currentDay = date.getDay();
 dayTextEl.textContent = days[currentDay];
